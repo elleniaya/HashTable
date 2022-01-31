c58c32e4d10fd77d746bb42a2b88e1eca72446c5
Елена Евмененко <98080553+elleniaya@users.noreply.github.com>
2022-02-01 00:49:03 +0700

    Renamed the resize_Table function to resize_table. Fixed comments to the insert method

--- hashtable.h
+++ hashtable.h
@@ -32,9 +32,9 @@ public:
 	//If the value with the key k, in the table exists, it will be removed and return true, otherwise false.
 	bool erase(const Key& k);
 
-	//Inserting a value pair <k, v>.
-	//If there is already an element with the same key in the table, replace it.
-    // CR: write when true / false returned
+	//Insert a pair of values <k, v>.
+        //If no value with this key is found in the table, insert the value pair <k, v> and return true.
+        //If the table has a value with this key, replace it and return false.
 	bool insert(const Key& k, const Value& v);
 
 	//Search for a value, given key k. 
@@ -61,7 +61,7 @@ public:
 	friend bool operator!=(const HashTable& a, const HashTable& b);
 private:
 	int hash_func(const Key& s) const;
-	void resize_Table(size_t new_size);
+	void resize_table(size_t new_size);
 	Value& const_at(const Key& k) const;
 	void copy_table(const HashTable& a, const HashTable& b);
 
@@ -69,7 +69,6 @@ private:
 
 	size_t size_;
 	size_t size_vector_table;
-	static const size_t start_size = 100;
-    static constexpr double RESIZE_CONST = 0.3;
-	//static const double RESIZE_CONST;
+	static const size_t start_size = 8;
+	static constexpr double RESIZE_CONST = 0.3;
 };
