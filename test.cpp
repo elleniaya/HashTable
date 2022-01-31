fbe278e1d4c2a3535691e36f4db8d2a926533dee
Елена Евмененко <98080553+elleniaya@users.noreply.github.com>
2022-02-01 00:56:36 +0700

    Added tests for resize_table, insert, operator[] and operator==.

--- test.cpp
+++ test.cpp
@@ -1,4 +1,3 @@
-#include "pch.h"
 #include "hashtable.h"
 
 TEST(HashTable, ClearCheck) {
@@ -23,9 +22,14 @@ TEST(HashTable, InsertCheck) {
 	Value value2("Aria", 19);
 
 	test.insert("393939", value1);
-	test.insert("393939", value2);
-	ASSERT_EQ(test["393939"].age, value2.age);
-	ASSERT_EQ(test["393939"].name, value2.name);
+	test.insert("200939", value2);
+
+	ASSERT_EQ(test.size(), 2);
+	ASSERT_EQ(test["393939"].age, value1.age);
+	ASSERT_EQ(test["393939"].name, value1.name);
+
+	ASSERT_EQ(test["200939"].age, value2.age);
+	ASSERT_EQ(test["200939"].name, value2.name);
 }
 
 TEST(HashTable, InsertReplaceCheck) {
@@ -33,7 +37,8 @@ TEST(HashTable, InsertReplaceCheck) {
 	Value value1("Bella", 18);
 	Value value2("Edward", 28);
 	test.insert("7777", value1);
-	test.insert("7777", value2);
+
+	ASSERT_FALSE(test.insert("7777", value2));
 
 	ASSERT_EQ(test["7777"].age, value2.age);
 	ASSERT_EQ(test["7777"].name, value2.name);
@@ -43,11 +48,17 @@ TEST(HashTable, InsertReplaceCheck) {
 TEST(HashTable, ContainsTrueCheck) {
 	HashTable test;
 
-	Value value("Richard", 57);
+	Value value1("Richard", 57);
+	Value value2("Larry", 3);
+	Value value3("Olivia", 23);
 
-	test.insert("3297284", value);
+	test.insert("3297284", value1);
+	test.insert("88839", value2);
+	test.insert("10101010", value3);
 
 	ASSERT_TRUE(test.contains("3297284"));
+	ASSERT_TRUE(test.contains("88839"));
+	ASSERT_TRUE(test.contains("10101010"));
 }
 
 TEST(HashTable, ContainsFalseCheck) {
@@ -71,6 +82,7 @@ TEST(HashTable, EraseTrueCheck) {
 
 TEST(HashTable, EraseFalseCheck) {
 	HashTable test;
+
 	ASSERT_FALSE(test.erase("84385"));
 }
 
@@ -86,11 +98,13 @@ TEST(HashTable, SizeCheck) {
 	Value value4("Victor", 15);
 	test.insert("9339200000", value4);
 	test.erase("9339200000");
+
 	ASSERT_EQ(test.size(), 3);
 }
 
 TEST(HashTable, SizeNullCheck) {
 	HashTable test;
+
 	ASSERT_EQ(test.size(), 0);
 }
 
@@ -185,6 +199,10 @@ TEST(HashTable, OperatorAssignmentCheck) {
 	test2 = test1;
 
 	ASSERT_EQ(test1, test2);
+
+	ASSERT_TRUE(test2.contains("8202173"));
+	ASSERT_TRUE(test2.contains("6664666"));
+
 	ASSERT_FALSE(test2.contains("89992173"));
 	ASSERT_FALSE(test2.contains("68632000"));
 	ASSERT_FALSE(test2.contains("8888"));
@@ -204,6 +222,22 @@ TEST(HashTable, OperatorEqualityCheck) {
 	test2.insert("6666666", value4);
 
 	ASSERT_TRUE(test1 == test2);
+}
+
+TEST(HashTable, OperatorEqualityFalseCheck) {
+	HashTable test1;
+	Value value1("Inna", 20);
+	test1.insert("13579", value1);
+	Value value2("Elena", 18);
+	test1.insert("6666666", value2);
+
+	HashTable test2;
+	Value value3("Mary", 5);
+	test2.insert("13579", value3);
+	Value value4("Ava", 27);
+	test2.insert("6666666", value4);
+
+	ASSERT_FALSE(test1 == test2);
 }
 
 TEST(HashTable, OperatorInequalityCheck) {
@@ -236,7 +270,6 @@ TEST(HashTable, OperatorInequalityCheck1) {
 	ASSERT_TRUE(test1 != test2);
 }
 
-
 TEST(HashTable, OperatorIndexCheck) {
 	HashTable test;
 	Value first("Sara", 50);
@@ -247,10 +280,61 @@ TEST(HashTable, OperatorIndexCheck) {
 	ASSERT_EQ(test["757474"].name, first.name);
 }
 
-TEST(HashTable, OperatorIndexNullCheck) {
+TEST(HashTable, OperatorIndexNullCheck1) {
 	HashTable test;
 	Value val("", 0);
 
 	ASSERT_EQ(test["8888"].name, val.name);
 	ASSERT_EQ(test["8888"].age, val.age);
+}
+
+TEST(HashTable, OperatorIndexNullCheck2) {
+	HashTable test;
+	Value v1("Jonny", 67);
+	test.insert("888", v1);
+	Value val("", 0);
+
+	ASSERT_EQ(test["928366600"].name, val.name);
+	ASSERT_EQ(test["928366600"].age, val.age);
+}
+
+TEST(HashTable, Resize_Check) {
+	HashTable test;
+	Value v1("Jonny", 67);
+	Value v2("Lily", 20);
+	Value v3("Lion", 5);
+	Value v4("Margo", 27);
+	Value v5("Jinny", 15);
+	Value v6("Piter", 90);
+	test.insert("888", v1);
+	test.insert("928366600", v2);
+	test.insert("2993746", v3);
+	test.insert("742749", v4);
+	test.insert("82479427", v5);
+	test.insert("10", v6);
+
+	ASSERT_TRUE(test.contains("888"));
+	ASSERT_TRUE(test.contains("928366600"));
+	ASSERT_TRUE(test.contains("2993746"));
+	ASSERT_TRUE(test.contains("742749"));
+	ASSERT_TRUE(test.contains("82479427"));
+	ASSERT_TRUE(test.contains("10"));
+
+	ASSERT_EQ(test["888"].age, v1.age);
+	ASSERT_EQ(test["888"].name, v1.name);
+
+	ASSERT_EQ(test["928366600"].age, v2.age);
+	ASSERT_EQ(test["928366600"].name, v2.name);
+
+	ASSERT_EQ(test["2993746"].age, v3.age);
+	ASSERT_EQ(test["2993746"].name, v3.name);
+
+	ASSERT_EQ(test["742749"].age, v4.age);
+	ASSERT_EQ(test["742749"].name, v4.name);
+
+	ASSERT_EQ(test["82479427"].age, v5.age);
+	ASSERT_EQ(test["82479427"].name, v5.name);
+
+	ASSERT_EQ(test["10"].age, v6.age);
+	ASSERT_EQ(test["10"].name, v6.name);
 }
